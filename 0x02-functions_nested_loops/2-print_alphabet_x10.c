ad9eb9aa39664aaa2710180a782657f06bdc5da1
segunojo1 <olusegunkd2017@gmail.com>
2023-08-15 18:17:27 -0700

    print 10times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,9 @@
 
 void print_alphabet_x10(void)
 {
-	char letter;
+	int num;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (num = 0; num <= 10; num++)
 	{
 		char letter2;
 
@@ -16,5 +16,6 @@ void print_alphabet_x10(void)
 		{
 			_putchar(letter2);
 		}
+		_putchar('\n');
 	}
 }
