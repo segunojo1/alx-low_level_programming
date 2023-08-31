22c60ca97b79cfb38be66ab1d84e064130bb46dd
segunojo1 <olusegunkd2017@gmail.com>
2023-08-31 02:04:14 -0700

    betty err

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
  * _strlen_recursion - finds the length of a string
- *
+ *@s : string to be counted
+ *Return: length of string
  */
 int _strlen_recursion(char *s)
 {
 	int len = 0;
+
 	if (*s)
 	{
 		len++;
-		len =+ _strlen_recursion(s + 1);
+		len += _strlen_recursion(s + 1);
 	}
-	return len;
+	return (len);
 }
