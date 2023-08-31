f2269bb2bbbeee45791f34d6f803abcd26ebc59e
segunojo1 <olusegunkd2017@gmail.com>
2023-08-31 02:09:27 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a number
+ * @n: numbeer to get factorial
+ * Return: factorial of the num
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return -1;
+	}
+	if (n == 1)
+	{
+		return 1;
+	}
+	return n * factorial(n - 1);
+}
