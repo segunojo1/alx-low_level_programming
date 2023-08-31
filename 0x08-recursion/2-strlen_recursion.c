c0a8b6c54e9333a60dcab679994e5dc8ce77c6c0
segunojo1 <olusegunkd2017@gmail.com>
2023-08-31 01:58:48 -0700

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion - finds the length of a string
+ *
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+	if (*s)
+	{
+		len++;
+		_strlen_recursion(s + 1);
+	}
+	return len;
+}
