be1d4140068b3404ef25780b1aec6c7d748d7259
segunojo1 <olusegunkd2017@gmail.com>
2023-09-08 16:17:58 -0700

    remove space

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -67,12 +67,10 @@ int main(int argc, char *argv[])
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
-
 	if (!result)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
