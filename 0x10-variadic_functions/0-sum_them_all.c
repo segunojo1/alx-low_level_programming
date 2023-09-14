62ed0a97c6c7d34d8c372db3ffedb70905bb4126
segunojo1 <olusegunkd2017@gmail.com>
2023-09-14 08:47:02 -0700

    move declarations to top

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,17 +8,15 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list numbers;
+	unsigned int i, result = 0;
+
 	if (n == 0)
 	{
 		return (0);
 	}
 
-	va_list numbers;
-
 	va_start(numbers, n);
-
-	unsigned int i;
-	unsigned int result = 0;
 
 	for (i = 0; i < n; i++)
 	{
