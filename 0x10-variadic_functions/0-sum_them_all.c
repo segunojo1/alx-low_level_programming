f843d75db929c52c3fb45a0805fd330dc46047fd
segunojo1 <olusegunkd2017@gmail.com>
2023-09-14 08:19:23 -0700

    add unsigned int

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -22,7 +22,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (i = 0; i > n; i++)
 	{
-		result = result + va_arg(numbers, int);
+		result = result + va_arg(numbers, unsigned int);
 	}
 
 	return (result);
