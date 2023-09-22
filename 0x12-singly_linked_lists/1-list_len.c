1f9873a2f2469d32daccfec9c0201b941dd7f42c
segunojo1 <olusegunkd2017@gmail.com>
2023-09-21 17:41:01 -0700

    namees

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,20 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * listint_len - returns no of elements in a linked lists
- * @h: linked list of type listint_t to be traversed
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
  *
- * Return: no of nodes
+ * Return: number of elements in h
  */
-size_t listint_len(const listint_t *h)
+size_t list_len(const list_t *h)
 {
-	size_t num = 0;
+ size_t n = 0;
 
-	while (h)
-	{
-		num++;
-		h = h->next;
-	}
-
-	return (num);
+ while (h)
+ {
+ n++;
+ h = h->next;
+ }
+ return (n);
 }
