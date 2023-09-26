b22a19f21e61cbad90585ea73af32cc20067edc7
segunojo1 <olusegunkd2017@gmail.com>
2023-09-26 18:50:37 +0100

    err

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-        int i;
+        unsigned int i;
         listint_t *temp;
         temp = *head;
         listint_t *newNode;
