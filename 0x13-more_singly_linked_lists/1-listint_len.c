e321d37e1c5cb842281651851368886bf512e8b0
segunojo1 <olusegunkd2017@gmail.com>
2023-09-26 05:42:27 -0700

    1 betty

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,16 +2,16 @@
 /**
  * listint_len - number of elems in the list
  * @h: head of list
- * 
+ *
  * Return: returns the no of elems in the list
-*/
+ */
 size_t listint_len(const listint_t *h)
 {
-        size_t s = 0;
-        while (h)
-        {
-                h = h->next;
-                s++;
-        }
-        return (s);
+	size_t s = 0;
+	while (h)
+	{
+		h = h->next;
+		s++;
+	}
+	return (s);
 }
