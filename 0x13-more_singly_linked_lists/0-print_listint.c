127830a1724854bb218096825668c1cc0d6f49e9
segunojo1 <olusegunkd2017@gmail.com>
2023-09-26 12:28:49 +0100

    task 0 and others

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,4 +3,14 @@
 
 size_t print_listint(const listint_t *h)
 {
+        size_t s = 0;
+        while (h)
+        {
+                
+                printf("%i", h->n);
+                h = h->next;
+                s++;
+        }
+        return (s);
+}
         
