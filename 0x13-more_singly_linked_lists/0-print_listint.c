e3740a7d0ba659b2311442d62a4013a606c15ecc
segunojo1 <olusegunkd2017@gmail.com>
2023-09-26 12:30:17 +0100

    add comment

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_listint - prints the members of the linked list
+ * h: member of the linked list
+ * 
+ * Return: number of elems in the list
+*/
 size_t print_listint(const listint_t *h)
 {
         size_t s = 0;
