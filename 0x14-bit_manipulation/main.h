e7e3b84ed9f71095bf4de720d7d0e06664869367
segunojo1 <olusegunkd2017@gmail.com>
2023-09-28 08:38:39 -0700

    add putchar func

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,3 +1,5 @@
+main.h
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -7,5 +9,8 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int _atoi(const char *s);
+int _putchar(char c);
 int get_endianness(void);
+
 #endif
